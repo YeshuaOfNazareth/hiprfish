1c593d0c1aba4191c69cc85dbd04a3cc5134cfa9
YeshuaOfNazareth <mirmammadjavadzada@gmail.com>
2022-04-28 16:47:35 -0400

    deleted probe-design and image-analysis

--- image-analysis/hiprfish-image-analysis-ecoli/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
-import numpy as np
-
-setup(
-    ext_modules = cythonize('neighbor2d.pyx', annotate = True),
-    include_dirs = [np.get_include()]
-)
